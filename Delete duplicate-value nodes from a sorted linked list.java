33fcb7cd34fa40af208d546365ae2ee3fd09261b
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:30:23 -0500

    Update Delete duplicate-value nodes from a sorted linked list.java

--- Delete duplicate-value nodes from a sorted linked list.java
+++ Delete duplicate-value nodes from a sorted linked list.java
@@ -10,28 +10,28 @@
      *
      */
     static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
-        SinglyLinkedListNode aux = head;
-        SinglyLinkedListNode ant = null;
+        SinglyLinkedListNode aux = head;  // inicializamos un nodo que apunte al inicio
+        SinglyLinkedListNode ant = null;  // inicializamos con nulo un nodo anterior
 
-        if(head.next == null){
-            return head;
+        if(head.next == null){  // si el inicio no tiene nodos siguientes
+            return head;  // regresamos la cabeza
         }
         else{
 
-            while(aux.next != null){
-                ant = aux;
-                aux = aux.next;
+            while(aux.next != null){   // mientras tengamos un nodo siguiente
+                ant = aux;   // movemos anterior a donde está auxiliar
+                aux = aux.next;  // movemos auxiliar al nodo siguiente
 
-                while(aux.data == ant.data){
-                    if(aux.next == null){
-                        aux = null;
-                        ant.next = aux;
-                        return head;
+                while(aux.data == ant.data){  // si los datos de ambos nodos son iguales
+                    if(aux.next == null){  // y no hay nodo siguiente
+                        aux = null;  // auxiliar lo volvemos nulo
+                        ant.next = aux;  // conectamos el nodo anterior con el auxiliar
+                        return head;  // regresamos la cabeza
                     }
-                    aux = aux.next;
+                    aux = aux.next;  // si no, que avance al nodo siguiente
                 }
 
-            ant.next = aux;
+            ant.next = aux;  // conectamos el nodo anterior con auxiliar
 
             }
         }
