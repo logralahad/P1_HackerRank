faf7937788b693a46dfa8eeda3052ab4dfecff70
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:44:22 -0500

    Update Insert a node at the head of a linked list.java

--- Insert a node at the head of a linked list.java
+++ Insert a node at the head of a linked list.java
@@ -11,13 +11,13 @@
      */
     static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
         
-        SinglyLinkedListNode aux = new SinglyLinkedListNode(data);
+        SinglyLinkedListNode aux = new SinglyLinkedListNode(data);  // creamos el nuevo nodo con dato como valor
 
-        if(llist != null){
-            aux.next = llist;
-            return aux;
+        if(llist != null){ // si la lista no está vacia
+            aux.next = llist;  // conectamos a nuestro nuevo nodo con la cabeza, siendo éste la nueva cabeza
+            return aux; // regresamos el nuevo nodo (que es la nueva cabeza)
         }
         
-        llist = new SinglyLinkedListNode(data);
-        return llist;
+        llist = new SinglyLinkedListNode(data);  /// si no, la cabeza será el nuevo nodo
+        return llist;  // regresamos el inicio
     }
