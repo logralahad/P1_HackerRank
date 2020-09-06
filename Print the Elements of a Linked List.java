f0424d287e3980fd7f6afd1f7b4c77cb4a9e421e
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:50:38 -0500

    Update Print the Elements of a Linked List.java

--- Print the Elements of a Linked List.java
+++ Print the Elements of a Linked List.java
@@ -13,11 +13,11 @@
      */
     static void printLinkedList(SinglyLinkedListNode head) {
 
-        SinglyLinkedListNode aux = head;
+        SinglyLinkedListNode aux = head;  // inicializamos un nodo auxiliar que apunte al inicio
         
-        while(aux != null){
-            System.out.println(aux.data);
-            aux = aux.next;
+        while(aux != null){  // mientras no lleguemos al fin de la lista
+            System.out.println(aux.data);  // imprimimos el valor de ese nodo
+            aux = aux.next;  // auxiliar lo movemos al siguiente nodo
         }
 
 
