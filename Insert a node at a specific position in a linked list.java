2cebfc1b1eb110938a50d5e07cc401e32484e3fe
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:42:34 -0500

    Update Insert a node at a specific position in a linked list.java

--- Insert a node at a specific position in a linked list.java
+++ Insert a node at a specific position in a linked list.java
@@ -11,24 +11,24 @@
      */
     static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
         
-        SinglyLinkedListNode nuevo = new SinglyLinkedListNode(data);
+        SinglyLinkedListNode nuevo = new SinglyLinkedListNode(data);  // creamos el nuevo nodo con dato como valor
 
-        if(head == null){
-            head = new SinglyLinkedListNode(data);
+        if(head == null){ // si la lista está vacía
+            head = new SinglyLinkedListNode(data); // el nuevo nodo será nuestra cabeza
             return head;
         }
         else{
-            SinglyLinkedListNode ant = null;
-            SinglyLinkedListNode aux = head;
+            SinglyLinkedListNode ant = null;  // inicializamos un nodo anterior a nulo
+            SinglyLinkedListNode aux = head;  // inicializamos un nodo auxiliar que apunte a inicio
 
 
-            while(aux != null && position > 0){
-                ant = aux;
-                aux = aux.next;
-                position--;
+            while(aux != null && position > 0){ // mientras no hayamos llegado a la posicion y auxiliar no sea nulo
+                ant = aux; // anterior lo movemos a donde estaba auxiliar
+                aux = aux.next; // auxiliar lo avanzamos un nodo siguiente
+                position--;  // le quitamos uno a la posicion
             }
-            ant.next = nuevo;
-            nuevo.next = aux;
+            ant.next = nuevo;  // conectamos el nodo anterior con el nuevo nodo
+            nuevo.next = aux;  // conectamos el nuevo nodo a auxiliar
         }
         return head;
 
