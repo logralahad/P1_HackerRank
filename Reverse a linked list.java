6c922440304d713e0875cdc4506e45cd0f130e32
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:57:18 -0500

    Update Reverse a linked list.java

--- Reverse a linked list.java
+++ Reverse a linked list.java
@@ -11,47 +11,47 @@
      */
     static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
 
-        if(head.next == null){
-            return head;
+        if(head.next == null){  // si la lista está vacía
+            return head;  // regresamos la cabeza
         }
 
         else{
 
-            SinglyLinkedListNode aux = head;
-            SinglyLinkedListNode ant = null;
-            SinglyLinkedListNode top = null;
+            SinglyLinkedListNode aux = head;   // inicializamos el nodo auxiliar al inicio
+            SinglyLinkedListNode ant = null;  // inicializamos un nodo anterior
+            SinglyLinkedListNode top = null;  // inicializamos un nodo top
 
 
-            while(aux != null){
-                ant = aux;
-                aux = aux.next;
+            while(aux != null){   // mientras no lleguemos al final de la lista
+                ant = aux;  // movemos el nodo anterior a donde estaba auxiliar
+                aux = aux.next;  // movemos auxiliar al siguiente nodo
             }
 
-            aux = head;
-            head = ant;
-            top = aux;
+            aux = head;  // auxiliar lo movemos a la cabeza
+            head = ant;  // la cabeza será el nodo anterior
+            top = aux;  //  y el nodo top será auxiliar, que es la cabeza
 
 
             do{
-
-                while(aux.next != null && aux.next != ant ){
-                    aux = aux.next;
+ 
+                while(aux.next != null && aux.next != ant ){  // mientras haya un nodo siguiente y ese nodo sea diferente a anterior
+                    aux = aux.next;  // movemos auxiliar al siguiente nodo
                 }
 
-                if(head.next == null){
-                    aux.next = null;
-                    head.next = aux;
-                    ant = head.next;
+                if(head.next == null){  // si no hay otro nodo despues de la cabeza
+                    aux.next = null;  // volvemos nulo al nodo siguiente de auxiliar
+                    head.next = aux;  // conectamos la cabeza con auxiliar
+                    ant = head.next;  // anterior lo hacemos el nodo siguiente a la cabeza
                 }else{
-                    aux.next = null;
-                    ant.next = aux;
-                    ant = aux;
+                    aux.next = null;  // el nodo siguiente de auxiliar lo hacemos nulo
+                    ant.next = aux;  // el nodo siguiente de anterior será auxiliar
+                    ant = aux; // movemos auxiliar a donde estaba auxiliar
                 }
 
-                aux = top;
+                aux = top;  // auxiliar lo hacemos el nodo top
                 
-            }while(ant != top);
+            }while(ant != top);  // todo mientras el nodo anterior sea diferente al nodo top
 
-            return head;
+            return head;  // regresamos el nodo cabeza
         }
     }
