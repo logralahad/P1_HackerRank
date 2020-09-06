5a45e81e1ede12152d8fcff5b17397c12dbf4b7b
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:24:35 -0500

    Update Cycle Detection.java

--- Cycle Detection.java
+++ Cycle Detection.java
@@ -11,21 +11,21 @@
      */
     static boolean hasCycle(SinglyLinkedListNode head) {
         
-        if(head == null) return false;
+        if(head == null) return false;  // si el inicio es nulo, entonces está vacía y retorna falso
 
         
-        SinglyLinkedListNode ant = head;
-        SinglyLinkedListNode aux = head.next;
+        SinglyLinkedListNode ant = head; // declaramos un nodo anterior en el inicio de la lista
+        SinglyLinkedListNode aux = head.next;  // declaramos un nodo auxiliar en el nodo siguiente al inicio de la lista
 
-        while(aux != null && aux.next != null){
+        while(aux != null && aux.next != null){  // mientras no llegue a nulo
 
-            if(ant == aux){
+            if(ant == aux){   // si el dato del nodo anterior es el mismo al de auxiliar, regresa verdadero
                 return true;
             }
 
-            ant = ant.next;
-            aux = aux.next.next;
+            ant = ant.next;  // avanza al siguiente del nodo anterior
+            aux = aux.next.next;  // avanza dos nodos hacia adelante del nodo auxiliar
         }
 
-        return false;
+        return false;  // si no se cumplió, regresa falso
     }
