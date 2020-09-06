c4d24dc2a8650577220b68e808db9672747f1c82
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:36:13 -0500

    Update Get Node Value.java

--- Get Node Value.java
+++ Get Node Value.java
@@ -10,26 +10,26 @@
      *
      */
     static int getNode(SinglyLinkedListNode head, int positionFromTail) {
-        SinglyLinkedListNode aux = head;
-        int pos_tail = 0;
-        int value = 0;
+        SinglyLinkedListNode aux = head;   // inicializamos un nodo auxiliar al inicio de la lista
+        int pos_tail = 0;  // creamos un contador de posiciones
+        int value = 0;  // creamos el valor a comparar
 
-        while(aux.next != null){
-            aux = aux.next;
-            pos_tail++;
+        while(aux.next != null){   // si hay un nodo siguiente
+            aux = aux.next;  // avanzamos a ese nodo
+            pos_tail++;  // al contador le sumamos uno
         }
 
-        if(positionFromTail == 0){
-            value = aux.data;
+        if(positionFromTail == 0){  // si nuestra posición es el final
+            value = aux.data;  // el nuevo valor es el de auxiliar
         }
         else{
-            aux = head;
-            pos_tail -= positionFromTail;
-            while(pos_tail != 0 && aux.next != null){
-                aux = aux.next;
-                pos_tail--;
+            aux = head;  // si no, auxiliar lo movemos al inicio
+            pos_tail -= positionFromTail;   // a la posicion desde el inicio le restamos la posicion desde la cola
+            while(pos_tail != 0 && aux.next != null){  // mientras la posicion desde la cola no sea 0 y haya un nodo siguiente
+                aux = aux.next;  // nos movemos al siguiente nodo
+                pos_tail--;  // y le quitamos uno a la posicion desde la cola
             }
-            value = aux.data;
+            value = aux.data;  // el nuevo valor será el valor de auxiliar
         }
-        return value;
+        return value;  // regresamos ese nuevo valor
     }
