1763017872c6648a58e8d42a5a7a5833b72bc781
logralahad <61020509+logralahad@users.noreply.github.com>
2020-09-06 16:32:47 -0500

    Update Find Merge Point of Two Lists.java

--- Find Merge Point of Two Lists.java
+++ Find Merge Point of Two Lists.java
@@ -11,26 +11,26 @@
      */
     static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
 
-        SinglyLinkedListNode aux1 = head1;
-        SinglyLinkedListNode aux2 = head2;
+        SinglyLinkedListNode aux1 = head1;  // inicializamos un nodo auxiliar que apunte al inicio de la lista 1
+        SinglyLinkedListNode aux2 = head2;  // inicializamos un nodo auxiliar que apunte al inicio de la lista 2
 
-        while(aux1 != aux2){
+        while(aux1 != aux2){  // si son diferentes
 
-            if(aux1.next == null){
-                aux1 = head2;
+            if(aux1.next == null){  // y no hay un nodo después
+                aux1 = head2;  // auxiliar1 lo volvemos la cabeza de la lista 2
             }else{
-                aux1 = aux1.next;
+                aux1 = aux1.next;  // si no, avanzamos al siguiente nodo
             }
 
-            if(aux2.next == null){
-                aux2 = head1;
+            if(aux2.next == null){  // si no hay un nodo después del auxiliar2
+                aux2 = head1; // auxiliar2 lo volvemos la cabeza de la lista 1
             }else{
-                aux2 = aux2.next;
+                aux2 = aux2.next; // si no, avanzamos al siguiente nodo
             }
 
         }
 
-        return aux1.data;
+        return aux1.data;  // regresamos el dato del nodo auxiliar1
 
     }
 
